499f7a9da773f68b8cb37191349966299bc3e30b
LBoll <lboll582@gmail.com>
2018-01-12 13:06:23 -0500

    first commit

---
+++ KnockAndCheckHF/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KnockAndCheckHF.Startup))]
+namespace KnockAndCheckHF
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
